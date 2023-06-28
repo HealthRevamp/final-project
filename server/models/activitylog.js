dc86e3644e550e118378dd8f52d6ade745fdb537
Hendro Sinambela <hendrosinambela19@gmail.com>
2023-06-28 19:30:09 +0700

    setup part3

---
+++ server/models/activitylog.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class ActivityLog extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  ActivityLog.init({
+    calorieBurned: DataTypes.INTEGER,
+    timeElapsed: DataTypes.DATE,
+    isCompleted: DataTypes.BOOLEAN,
+    UserId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'ActivityLog',
+  });
+  return ActivityLog;
+};
